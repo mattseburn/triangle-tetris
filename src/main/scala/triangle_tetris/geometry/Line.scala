e04ae99fd8b82b4a6891b013f65af98190a56fad
Matt Seburn <matt@seburn.net>
2021-01-17 02:53:14 -0500

    clean up

--- src/main/scala/triangle_tetris/geometry/Line.scala
+++ src/main/scala/triangle_tetris/geometry/Line.scala
@@ -21,19 +21,13 @@ case class Line(p1: Point, p2: Point) extends Primitive(location = p1) with Tran
     val b = x3*y4 - y3*x4
     val c = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
 
-    println(s"intersection of $this and $l")
-
     c match {
       case 0 => None
       case _ =>
         val x = (a * (x3 - x4) - b * (x1 - x2)) / c
         val y = (a * (y3 - y4) - b * (y1 - y2)) / c
 
-        val p = Point(x, y)
-
-        println(s"result: $p")
-
-        Some(p)
+        Some(Point(x, y))
     }
   }
 
@@ -43,17 +37,11 @@ case class Line(p1: Point, p2: Point) extends Primitive(location = p1) with Tran
   def ==(l: Line): Boolean =
     Set(p1, p2) == Set(l.p1, l.p2)
 
-  def transpose(delta: Point): Line = {
-    val l = Line(p1 + delta, p2 + delta)
-    println(s"transposing $this by $delta, result: $l")
-    l
-  }
+  def transpose(delta: Point): Line =
+    Line(p1 + delta, p2 + delta)
 
-  def rotate(angle: Double): Line = {
-    val l =Line(p1.rotate(angle), p2.rotate(angle))
-    println(s"rotating $this by $angle, result: $l")
-    l
-  }
+  def rotate(angle: Double): Line =
+    Line(p1.rotate(angle), p2.rotate(angle))
 
   def resize(size: Double): Line =
     Line(p1, p2 * (size / length))
