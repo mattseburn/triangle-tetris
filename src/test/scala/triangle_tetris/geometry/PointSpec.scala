e19ed9f05db9229e3b24c7920181d6aededd0e3c
Matt Seburn <matt@seburn.net>
2021-01-25 21:37:17 -0500

    clean up tests

--- src/test/scala/triangle_tetris/geometry/PointSpec.scala
+++ src/test/scala/triangle_tetris/geometry/PointSpec.scala
@@ -2,7 +2,6 @@ package triangle_tetris.geometry
 
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
-import scala.math._
 
 class PointSpec extends AnyWordSpec {
   "equality" when {
@@ -48,36 +47,6 @@ class PointSpec extends AnyWordSpec {
       val p2 = Point(3, 4)
 
       p1 - p2 shouldEqual Point(p1.x - p2.x, p1.y - p2.y)
-    }
-  }
-
-  "transposition" should {
-    "add the delta point" in {
-      val p1 = Point(1, 2)
-      val delta = Point(3, 4)
-
-      p1.transpose(delta) shouldEqual p1 + delta
-    }
-  }
-
-  "rotation" when {
-    "rotating about the origin" should {
-      "rotate by the given angle" in {
-        val p1 = Point(1, 0)
-        val angle = toRadians(90)
-
-        p1.rotate(angle) shouldEqual Point(0, 1)
-      }
-    }
-
-    "rotating about an arbitrary point" should {
-      "transpose to the origin before rotating and then back" in {
-        val p1 = Point(1, 0)
-        val angle = toRadians(90)
-        val pivot = Point(1, 1)
-
-        p1.rotate(pivot, angle) shouldEqual Point(2, 1)
-      }
     }
   }
 }
